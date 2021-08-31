5c5735e114eda1f2abc47c20eaecb7097741f8f9
TeenX <81262408+brute136@users.noreply.github.com>
2021-08-31 23:53:01 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+//1st Problem Solved In C++
+using namespace std;
+int main()
+{
+	cout<<"Hello\nWorld";
+	return 0; 
+}
---
+++ 2nd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//***2nd***//
+//**Sum Of Two Number  ***// 
+//** Sizeof Variable**//
+//***Sum Of Two Nuber Using Variable**//
+int main()
+{
+	cout<<"The Sum Of 10 + 40 is "<<10+40<<endl;
+	cout<<"The Sizeof Fun On Int Is "<<sizeof(int)<<endl;
+		cout<<"The Sizeof Funtion On long double Is "<<sizeof(long double)<<endl;
+		int x=20, b=40;
+		cout<<"Sum Using Variable "<< x+b<<endl;
+	
+	return 0; 
+}
